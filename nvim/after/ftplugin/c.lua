989dac2fac127e3af4c149206027738513313bca
cubeacon <wisamyassarmahardika@gmail.com>
2026-02-22 09:50:18 +0700

    improved nvim config

--- nvim/after/ftplugin/c.lua
+++ nvim/after/ftplugin/c.lua
@@ -1,35 +1,21 @@
 -- Enable C-specific automatic indenting
 vim.opt_local.cindent = true
 
--- Indentation Rules (matches most professional C projects)
-vim.opt_local.cinoptions = {
-    "g0",  -- Scope declarations (public/private) at 0 indent
-    ":0",  -- Switch case labels align with the switch statement
-    "(0",  -- Align continuation lines inside parentheses
-    "t0",  -- Return type of function at 0 indent (start of line)
-    "Ws",  -- Add extra indent for lines starting with an operator
-}
-
--- Tab & Spacing (Standard for large C projects/Glibc)
+-- Tab & Spacing (Standard for C)
 vim.opt_local.tabstop = 8
 vim.opt_local.shiftwidth = 8
 vim.opt_local.expandtab = false
 
--- Triggers for re-indenting while typing
-vim.opt_local.cinkeys = "0{,0},0),0],:,0#,!^F,o,O,e"
-
--- KEYBINDINGS: Built-in Documentation & Completion
--- K        : Open system man page for word under cursor
--- <C-x><C-o>: Trigger ctags-based completion (Omni-complete)
+-- DOCUMENTATION & REFERENCE (The "No-Tags" Way)
+-- K         : Open Linux Man Page for the function under cursor (e.g., printf, malloc)
+-- [i        : Show the first line where the word under cursor is defined (Fast!)
+-- [d        : Jump to the definition of a #define macro
+-- gf        : "Go to File" - Open the header file under the cursor
 vim.opt_local.keywordprg = ":Man"
-vim.opt_local.omnifunc = "ccomplete#Complete"
 
--- KEYBINDINGS: Navigation Helpers
--- <leader>h : Find and open the header (.h) for current .c file
--- <leader>c : Find and open the source (.c) for current .h file
--- CTRL-]    : Jump to definition (Standard Tag jump)
--- CTRL-t    : Jump back (Standard Tag pop)
--- g]         : Show all matching tags
+-- NAVIGATION HELPERS
 local opts = { buffer = true, silent = true }
+-- <leader>h : Switch to header (.h)
+-- <leader>c : Switch to source (.c)
 vim.keymap.set("n", "<leader>h", ":find %:t:r.h<CR>", opts)
 vim.keymap.set("n", "<leader>c", ":find %:t:r.c<CR>", opts)
--- nvim/init.lua
+++ nvim/init.lua
@@ -1,13 +1,9 @@
 -- NETRW & BASIC
 vim.cmd("let g:netrw_banner = 0")
-vim.opt.guicursor = ""
 vim.opt.nu = true
 vim.opt.relativenumber = true
 vim.opt.termguicolors = true
-vim.opt.swapfile = false
-vim.opt.backup = false
 vim.opt.undofile = true
-vim.opt.fileencoding = "utf-8"
 vim.opt.clipboard = "unnamedplus"
 
 -- SEARCH & INDENT
@@ -15,37 +11,18 @@ vim.opt.ignorecase = true
 vim.opt.smartcase = true
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
-vim.opt.autoindent = true
-vim.opt.smartindent = true
-vim.opt.wrap = true
 
 -- COLORSCHEME
 require("catppuccin").setup({ flavour = "macchiato" })
 vim.cmd.colorscheme("catppuccin-macchiato")
 
--- VANILLA C DEVELOPMENT (No-LSP)
--- Add current dir and system headers to path for :find and gf
+-- STANDARD LIBRARY REFERENCE CONFIG
+-- Add recursive project search and system headers to path
+-- This allows 'gf' on #include <stdio.h> to work instantly
 vim.opt.path:append("./**")
 vim.opt.path:append("/usr/include")
 
--- Ctags: looks for 'tags' file in current dir, then parent dirs upward
-vim.opt.tags = "./tags;,tags;"
-
--- KEYBINDINGS: Quickfix Navigation (for :make or :vimgrep results)
--- <C-n> : Go to next error/match
--- <C-p> : Go to previous error/match
-vim.keymap.set("n", "<C-n>", ":cnext<CR>", { desc = "Next Quickfix item" })
-vim.keymap.set("n", "<C-p>", ":cprev<CR>", { desc = "Previous Quickfix item" })
-
-vim.api.nvim_create_user_command('Ctags', function()
-    -- We use 'generate_tags.sh' directly since it's in your PATH
-    local output = vim.fn.system('generate_tags.sh')
-    
-    -- Check for errors (exit code non-zero)
-    if vim.v.shell_error ~= 0 then
-        print("Error updating ctags: " .. output)
-    else
-        print("Ctags updated successfully!")
-    end
-end, {})
-vim.g.editorconfig = "true"
+-- Quickfix Navigation (for :make or :vimgrep)
+-- <C-n> : Next error/match | <C-p> : Previous error/match
+vim.keymap.set("n", "<C-n>", ":cnext<CR>")
+vim.keymap.set("n", "<C-p>", ":cprev<CR>")
