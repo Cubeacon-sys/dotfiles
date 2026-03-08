7a81f4f751b6bfd782d1fa4f22122fb1b58d1c90
cubeacon <wisamyassarmahardika@gmail.com>
2026-03-08 09:36:07 +0700

    pywal and spice for vms

--- nvim/lua/plugins/catppuccin.lua
+++ nvim/lua/plugins/catppuccin.lua
@@ -2,10 +2,13 @@ return {
   {
     "catppuccin/nvim",
     name = "catppuccin",
-    priority = 1000, -- 1st priority
+    priority = 1000,
     config = function()
-      require("catppuccin").setup({ flavour = "macchiato" })
-      vim.cmd.colorscheme("catppuccin-macchiato")
+      require("catppuccin").setup({
+        flavour = "macchiato",
+        transparent_background = true,
+      })
+      vim.cmd.colorscheme("catppuccin")
     end,
   },
 }
